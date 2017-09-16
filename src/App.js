45cbe58370e76b4ff14135412aec6ace22895226
fiona <h.natapra@gmail.com>
2017-09-16 13:22:35 -0400

    added new component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ProductTile from  './components/Product.js'
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+function App() {
+  // array of products
+  const products = [
+        { id: 0,
+          name: "JavaScript: The Definitive Guide",
+          description: `Since 1996, JavaScript: The Definitive Guide...`,
+          price: 28.89
+        },
+        { id: 1,
+          name: "Eloquent JavaScript",
+          description: `JavaScript lies at the heart...`,
+          price: 21.22
+        },
+        { id: 2,
+          name: "JavaScript: The Good Parts",
+          description: `Most programming languages...`,
+          price: 16.59
+        }
+    ]
+
+    // render each item in products array using .map() ES6
+  return (
+
+    <section>
+      <h1>Products</h1>
+
+      { products.map( product =>
+        <ProductTile key={ product.id} item={ product} />
+      )}
+
+
+    </section>
+  )
 }
+
+
+
 
 export default App;
---
+++ src/components/Product.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function ProductTile(props) {
+  return(
+
+    <div>
+      <header>{ props.item.name }</header>
+      <p>
+        { props.item.description }
+      </p>
+      <span> ${ props.item.price }</span>
+    </div>
+
+  )
+
+}
+
+
+
+
+export default ProductTile;
